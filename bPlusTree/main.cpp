902f3efe64a038183fbdeba640c83b1d17da1df7
Artur Titkov <ArturTitkov@web.de>
2022-10-14 14:00:40 +0200

    Initial Commit

---
+++ bPlusTree/main.cpp
@@ -0,0 +1,23 @@
+//
+// Project:    bPlusTree
+// Filename:   main.cpp
+// Created on: 14.10.22
+//
+// Developer(s):
+//	I) Artur Titkov
+//	   ArturTitkov@icloud.com
+//	   https://github.com/TitkovArtur
+//
+// Description:
+//
+//
+// 
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
